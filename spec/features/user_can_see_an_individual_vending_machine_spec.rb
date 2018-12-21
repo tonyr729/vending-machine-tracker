3f8bf501dd183c5fbd6e37fa910be3e0e7d07147
Tony Robertson <tonyr729@gmail.com>
2018-12-21 11:01:14 -0700

    Add spec for average price

--- spec/features/user_can_see_an_individual_vending_machine_spec.rb
+++ spec/features/user_can_see_an_individual_vending_machine_spec.rb
@@ -1,23 +1,29 @@
 require 'rails_helper'
 
 feature 'When a user visits a vending machine show page' do
-  scenario 'they see the location of that machine' do
+  before(:each) do
     owner = Owner.create(name: "Sam's Snacks")
-    dons  = owner.machines.create(location: "Don's Mixed Drinks")
-    
-    visit machine_path(dons)
+    snack_1 = Snack.create(name: "Snickers", price: 1)
+    snack_2 = Snack.create(name: "Resses", price: 2)
+    snack_3 = Snack.create(name: "M&M", price: 1)
+    @dons  = owner.machines.create(location: "Don's Mixed Drinks", snacks: [snack_1, snack_2, snack_3])
+  end
+  scenario 'they see the location of that machine' do
+    visit machine_path(@dons)
     
     expect(page).to have_content("Don's Mixed Drinks Vending Machine")
   end
   
   scenario 'they see the name and price of all of the snacks' do
-    owner = Owner.create(name: "Sam's Snacks")
-    snack_1 = Snack.create(name: "Snickers", price: 1)
-    snack_2 = Snack.create(name: "Resses", price: 2)
-    snack_3 = Snack.create(name: "M&M", price: 1)
-    dons  = owner.machines.create(location: "Don's Mixed Drinks", snacks: [snack_1, snack_2, snack_3])
+    visit machine_path(@dons)
+    
+    expect(page).to have_content("Snickers $1")
+    expect(page).to have_content("Resses $2")
+    expect(page).to have_content("M&M $1")
+  end
 
-    visit machine_path(dons)
+  scenario 'they see the average price' do
+    visit machine_path(@dons)
     
     expect(page).to have_content("Snickers $1")
     expect(page).to have_content("Resses $2")
